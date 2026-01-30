cacba1eca6a5801b35d88a9a4085d092e000e4d3
Namrata2309 <ghayalnamrata239@gmail.com>
2026-01-30 14:36:47 +0530

    second

--- fronttend/src/api/api.js
+++ fronttend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://authapp-qvua.onrender.com",
   withCredentials: true, // REQUIRED for sessions
 });
 
