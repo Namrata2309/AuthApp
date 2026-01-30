ff3d9dd49e4b4731402641cf4ac7c3476acef22b
Namrata2309 <ghayalnamrata239@gmail.com>
2026-01-30 16:19:53 +0530

    /

--- fronttend/src/App.jsx
+++ fronttend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
